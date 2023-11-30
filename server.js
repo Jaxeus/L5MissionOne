b4d1c02245304de312510e6862eb4d48d6341d9b
Jason Cluley <jaxeus@gmail.com>
2023-11-30 14:28:55 +1300

    Changed res.send in both car value endpoints to res.json

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post("/carValue", (req, res) => {
   // const carValue = findCarValue(model, year);
   console.log("Console for req.body", req.body);
   console.log(`Console logging the car's value: $${carValue}`);
-  res.send({ carValue }); //{"carValue": 8708} where body = {"model": "Atenza", "year": 2008}
+  res.json({ carValue }); //{"carValue": 8708} where body = {"model": "Atenza", "year": 2008}
 });
 // ** To activate this endpoint, use Postman and send a POST request to http://localhost:4001/carValue with the body as raw and JSON format. The body should be {"model": "Atenza", "year": 2008} to get the same result as above comment. **
 // !! When using Postman, remember to change the body to raw and JSON format, rather than text. Hours spent on Text wondering why it wasn't working. Also remember to give the keys quotation marks. !!
@@ -101,7 +101,7 @@ app.post(
     // const carValue = findCarValue(model, year);
     console.log("Console for req.body", req.body);
     console.log(`Console logging the car's value: $${carValue}`);
-    res.send({ carValue }); //{"carValue": 8708} where body = {"model": "Atenza", "year": 2008}
+    res.json({ carValue }); //{"carValue": 8708} where body = {"model": "Atenza", "year": 2008}
   }
 );
 
